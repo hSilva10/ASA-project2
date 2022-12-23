2422689495039512009fbe2152b7aec62561acb4
Henrique Silva <hjapsilva@gmail.com>
2022-12-23 18:33:26 +0000

    fixed some error on the pdf and made created structures of Node and Edge

--- project_base.cpp
+++ project_base.cpp
@@ -1,4 +1,43 @@
- 
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+typedef vector<int> vec;
+
+typedef struct {
+     vec nodes;
+}Node;
+
+typedef struct {
+    int value;
+    Node *right;
+    Node *left;
+}Edge;
+
+void parse_input(vec nodes, vec edges);
+void get_max_tree();
+
+
+int main() {
+    vec nodes;
+    vec edges;
+    parse_input(&nodes, &edges);
     return 0;
+}
+
+void parse_input(vec nodes, vec edges){
+    int nodes_num;
+    int edges_num;
+
+    cin >> nodes_num;
+    cin >> edges_num;
+
+
+
+}
+void get_max_tree(){
+
 }
