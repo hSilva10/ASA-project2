1abdbf0222c5ef07e68004ad493c5d5854113ac8
Henrique Silva <hjapsilva@gmail.com>
2022-12-26 21:01:09 +0000

    added two implementations to the problem - base -> kruskal alt -> prim

--- project_alt.cpp
+++ project_alt.cpp
@@ -1,3 +1,81 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef struct {
+    int value;
+    int a, b;
+}Edge;
+
+typedef vector<Edge> Edges;
+
+void prim(Edges& edges, int num_vertices) {
+    // Initialize the list of included vertices
+    vector<bool> included(num_vertices, false);
+
+    // edges dimension
+    int size = edges.size();
+
+    // DEFAULT vertex 0 = true
+    included[0] = true;
+
+    // Add vertex 1 to the maximum weight spanning tree
+    included[1] = true;
+
+    int max_weight = 0;
+    while (true) {
+        // variable to see if all nodes are checked
+        bool change = false;
+
+        // Find the heaviest edge from an included vertex to a non-included vertex
+        int heaviest_edge_index = -1;
+        for (int i = 0; i < size; i++) {
+            if ((included[edges[i].a] && !included[edges[i].b]) ||
+                (included[edges[i].b] && !included[edges[i].a])){
+
+                if (heaviest_edge_index == -1 || edges[i].value > edges[heaviest_edge_index].value) {
+                    heaviest_edge_index = i;
+                }
+            }
+        }
+        // If no such edge was found, we have reached the end of the algorithm
+        if (heaviest_edge_index == -1){
+            for(int i = 0; i < num_vertices; i++)
+                if(!included[i]) {
+                    included[i] = true;
+                    change = true;
+                    break;
+                }
+        }
+        if(!change && heaviest_edge_index == -1)
+            break;
+        if(!change) {
+            // Add the heaviest edge to the maximum weight spanning tree
+            max_weight += edges[heaviest_edge_index].value;
+            if (!included[edges[heaviest_edge_index].a]) {
+                included[edges[heaviest_edge_index].a] = true;
+            } else {
+                included[edges[heaviest_edge_index].b] = true;
+            }
+        }
+    }
+
+    cout << max_weight << endl;
+}
+
 int main() {
-	return 0;
+    int nodes_num, edges_num;
+    cin >> nodes_num >> edges_num;
+
+    // Read in the edges
+    Edges edges;
+    for (int i = 0; i < edges_num; i++) {
+        int a, b, value;
+        cin >> a >> b >> value;
+        edges.push_back({value, a, b});
+    }
+
+    prim(edges, nodes_num);
+    return 0;
 }
--- project_base.cpp
+++ project_base.cpp
@@ -1,43 +1,95 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include <vector>
-#include <string.h>
+#include <algorithm>
 
 using namespace std;
 
-typedef vector<int> vec;
-
-typedef struct {
-     vec nodes;
-}Node;
-
 typedef struct {
     int value;
-    Node *right;
-    Node *left;
+    int a, b;
 }Edge;
 
-void parse_input(vec nodes, vec edges);
-void get_max_tree();
+typedef vector<Edge> Edges;
 
+struct Node {
+    int parent;
+    int rank;
+};
+
+class DisjointSetForest {
+public:
+    DisjointSetForest(int num_vertices) : nodes_(num_vertices) {
+        for (int i = 0; i < num_vertices; i++) {
+            nodes_[i].parent = i;
+            nodes_[i].rank = 0;
+        }
+    }
+
+    int find(int x) {
+        if (nodes_[x].parent != x) {
+            nodes_[x].parent = find(nodes_[x].parent);
+        }
+        return nodes_[x].parent;
+    }
+
+    void union_sets(int x, int y) {
+        int root_x = find(x);
+        int root_y = find(y);
+        if (root_x == root_y) return;
+
+        if (nodes_[root_x].rank < nodes_[root_y].rank) {
+            nodes_[root_x].parent = root_y;
+        } else if (nodes_[root_x].rank > nodes_[root_y].rank) {
+            nodes_[root_y].parent = root_x;
+        } else {
+            nodes_[root_y].parent = root_x;
+            nodes_[root_x].rank++;
+        }
+    }
+
+private:
+    vector<Node> nodes_;
+};
+
+void kruskal(Edges& edges, int num_vertices) {
+    // Sort the edges by weight in non-decreasing order
+    sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
+        return a.value > b.value;
+    });
+
+
+    // Initialize the disjoint-set forest data structure
+    DisjointSetForest parents(num_vertices);
+
+    int max_weight = 0;
+    for (const auto& edge : edges) {
+        // Find the root of the two vertices connected by this edge
+        int root_a = parents.find(edge.a);
+        int root_b = parents.find(edge.b);
+
+        // If the two vertices are not already in the same tree, add the edge
+        // to the maximum weight spanning tree
+        if (root_a != root_b) {
+            parents.union_sets(root_a, root_b);
+            max_weight += edge.value;
+        }
+    }
+
+    cout << max_weight << endl;
+}
 
 int main() {
-    vec nodes;
-    vec edges;
-    parse_input(&nodes, &edges);
+    int nodes_num, edges_num;
+    cin >> nodes_num >> edges_num;
+
+    // Read in the edges
+    Edges edges;
+    for (int i = 0; i < edges_num; i++) {
+        int a, b, value;
+        cin >> a >> b >> value;
+        edges.push_back({value, a, b});
+    }
+
+    kruskal(edges, nodes_num);
     return 0;
-}
-
-void parse_input(vec nodes, vec edges){
-    int nodes_num;
-    int edges_num;
-
-    cin >> nodes_num;
-    cin >> edges_num;
-
-
-
-}
-void get_max_tree(){
-
 }
